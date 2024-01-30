faa81e75574c4df87aa5a8b1f0b8b8439aa7f7d0
stg <a.sergiotorres@gmail.com>
2024-01-30 15:42:56 +0100

    reto 5

--- js/main.js
+++ js/main.js
@@ -33,8 +33,16 @@ por consola. Por ejemplo: si introduzco “Fernando”, me aparezca “Bienvenid
 (recuerda pasar de String a double con parseDouble). Usa la constante PI.
 */
 
-const pi = 3.1416;
-var radio = parseFloat(prompt("Dime el radio del círculo"))
+// const pi = 3.1416;
+// var radio = parseFloat(prompt("Dime el radio del círculo"))
 
-var area = pi * (radio ** 2)
-console.log("El área de un círculo de radio", radio, "es: ", area)
+// var area = pi * (radio ** 2)
+// console.log("El área de un círculo de radio", radio, "es: ", area)
+
+/*
+5. Lee un número por teclado e indica si es divisible entre 2 (resto = 0). Si no lo es, también
+debemos indicarlo.
+*/
+
+let num = parseInt(prompt("Escribe un número"))
+num % 2 == 0 ? console.log("Es divisible entre 2") : console.log("No es divisible entre 2")
