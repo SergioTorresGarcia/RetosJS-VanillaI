bb3454f6ac470556cff314e8319a045f6c94bc0d
stg <a.sergiotorres@gmail.com>
2024-01-30 15:34:22 +0100

    reto 1

---
+++ js/main.js
@@ -0,0 +1,14 @@
+/*
+1. Declara 2 variables numéricas (con el valor que desees), he indica cual es mayor de los
+dos. Si son iguales indicarlo también. Ves cambiando los valores para comprobar que
+funciona.
+*/
+
+var a = 8;
+var b = 5;
+
+if (a > b) {
+    console.log("A es la mayor")
+} else if (a < b) {
+    console.log("B es la mayor")
+} else console.log("A y B son iguales")
