ee1ff0536a32a82121a9d7ddf6650970382d051c
stg <a.sergiotorres@gmail.com>
2024-01-30 15:36:21 +0100

    reto 2

--- js/main.js
+++ js/main.js
@@ -4,11 +4,19 @@ dos. Si son iguales indicarlo también. Ves cambiando los valores para comprobar
 funciona.
 */
 
-var a = 8;
-var b = 5;
+// var a = 8;
+// var b = 5;
 
-if (a > b) {
-    console.log("A es la mayor")
-} else if (a < b) {
-    console.log("B es la mayor")
-} else console.log("A y B son iguales")
+// if (a > b) {
+//     console.log("A es la mayor")
+// } else if (a < b) {
+//     console.log("B es la mayor")
+// } else console.log("A y B son iguales")
+
+/* 
+2. Declara un String que contenga tu nombre, después muestra un mensaje de bienvenida
+por consola. Por ejemplo: si introduzco “Fernando”, me aparezca “Bienvenido Fernando”.
+*/
+
+var nombre = "Sergio";
+console.log("Bienvenido", nombre)
