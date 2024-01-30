2cc2bf1f11ef5f8788ca7ff7c4dc6cc15215baf1
stg <a.sergiotorres@gmail.com>
2024-01-30 17:35:18 +0100

    reto 10

--- js/main.js
+++ js/main.js
@@ -80,8 +80,34 @@ calcule el precio final con IVA. El IVA será una constante que sera del 21%
 que desees.
 */
 
-for (let num = 1; num <= 100; num++) {
-    if (num % 2 == 0 && num % 3 == 0) {
-        console.log(num);
-    }
-}
+// for (let num = 1; num <= 100; num++) {
+//     if (num % 2 == 0 && num % 3 == 0) {
+//         console.log(num);
+//     }
+// }
+
+/*
+10. Realiza una aplicación que nos pida un número de ventas a introducir, después nos
+pedirá tantas ventas por teclado como número de ventas se hayan indicado. Al final
+mostrará la suma de todas las ventas. Piensa que es lo que se repite y lo que no.
+*/
+
+let ventas = prompt("Cuantas compras has hecho? (escribe el número)")
+
+
+let total = 0;
+let veces = 0;
+do {
+    veces ++
+    let precio = parseInt(prompt("Cuánto has pagado?"));
+    total += precio;
+} while (veces < ventas)
+
+console.log("Has hecho ", ventas, "ventas y has pagado un total de", total+"€");
+
+// for (let i = 0; i++; i < ventas) {
+    
+//     console.log(resultado)
+// }
+
+
